b3929cf5c3e63cd539ce9d8217408f368360d80c
isgi arriza <isgiarriza@gmail.com>
2019-02-23 09:00:55 +0700

    init library

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as IDCard } from './src/IDCard';
+export { default as Self } from './src/Self';
+export { default as SelfIDCard } from './src/SelfIDCard';
---
+++ src/SelfIDCard.js
@@ -0,0 +1,74 @@
+'use strict';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'black',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Waiting</Text>
+  </View>
+);
+
+class App extends Component {
+  render() {
+    const { children: renderFn } = this.props;
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.front}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+        >
+          {(propsRnCamera) => {
+            if (propsRnCamera.status !== 'READY') return <PendingView />;
+            return (
+              <ImageBackground source={require('./assets/img/self_idcard.png')} style={{width:'100%', height: '100%'}}>
+                {renderFn ? renderFn(propsRnCamera) : null}
+              </ImageBackground>
+            );
+          }}
+        </RNCamera>
+      </View>
+    );
+  }
+
+  takePicture = async function (camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default App;
